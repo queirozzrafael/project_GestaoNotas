8721d52943afb9ceddcb8c67d1c1e2433a53521d
Nicolas Moretti <nicolasmorreti@gmail.com>
2024-04-25 20:46:25 -0300

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,15 @@
 function cadAlunos(){
     window.location = "register1.html"
 }
+function statusAprovacao() {
+    let mediaFinal = document.getElementById('mediaFinal').value;
+
+    if (mediaFinal >= 7) {
+        document.getElementById('status').innerText = 'Aprovado';
+    } else if (mediaFinal >= 3) {
+        document.getElementById('status').innerText = 'Recuperação';
+    } else {
+        document.getElementById('status').innerText = 'Reprovado';
+    }
+}
+
