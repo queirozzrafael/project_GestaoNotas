c37ffe0ea8fae0a6011a08f055f3f7a9905bf7d3
Nicolas Moretti <nicolasmorreti@gmail.com>
2024-04-26 20:40:52 -0300

    status de aprovação de nota

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,18 +1,6 @@
 function cadAlunos(){
     window.location = "register1.html"
 }
-function statusAprovacao() {
-    let mediaFinal = document.getElementById('mediaFinal').value;
-
-    if (mediaFinal >= 7) {
-        document.getElementById('status').innerText = 'Aprovado';
-    } else if (mediaFinal >= 3) {
-        document.getElementById('status').innerText = 'Recuperação';
-    } else {
-        document.getElementById('status').innerText = 'Reprovado';
-    }
-}
-
 function calcularMedias() {
     // Obter as notas dos inputs
     let notaProva1 = parseFloat(document.getElementById("notaProva1").value);
@@ -88,4 +76,14 @@ function calcularMedias() {
         <td>Ações</td>
     `;
 }
+function statusAprovacao() {
+    const mediaFinal = document.getElementById('mediaFinal').value;
 
+    if (mediaFinal >= 6) {
+        document.getElementById('status').innerText = 'Aprovado';
+    } else if (mediaFinal >= 3) {
+        document.getElementById('status').innerText = 'Recuperação';
+    } else {
+        document.getElementById('status').innerText = 'Reprovado';
+    }
+}
